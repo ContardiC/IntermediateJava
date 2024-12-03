98b1cb642b565825d4cfb2f04c91d48da069f26b
Carlo Contardi <c.contardi@spacecoding.it>
2024-12-03 16:24:21 +0100

    add constructor with parameters and gettters and setters

--- javapractice/src/main/java/it/spacecoding/javapractice/Car.java
+++ javapractice/src/main/java/it/spacecoding/javapractice/Car.java
@@ -1,9 +1,44 @@
 package it.spacecoding.javapractice;
 
 public class Car {
-    public String colour = "Black";
-
-    public Car(){
-        
+    // attibuti
+    private String mColour = "Black";
+    private int mNumberOfSeats = 4;
+    private String mInterior;
+    public int getNumberOfSeats() {
+        return mNumberOfSeats;
     }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        mNumberOfSeats = numberOfSeats;
+    }
+
+    // costruttore vuoto
+    public Car(){
+    }
+    // costruttore con parametri
+    public Car(String chosenColour, String chosenInterior){
+        mColour = chosenColour;
+        mInterior = chosenInterior;
+    }
+
+    public String getInterior() {
+        return mInterior;
+    }
+
+    public void setInterior(String interior) {
+        mInterior = interior;
+    }
+
+    public String getColour() {
+        return mColour;
+    }
+
+    public void setColour(String colour) {
+        mColour = colour;
+    }
+
+    //metodi
+    public void drive(){}
+
 }
--- javapractice/src/main/java/it/spacecoding/javapractice/CarSimulator.java
+++ javapractice/src/main/java/it/spacecoding/javapractice/CarSimulator.java
@@ -3,5 +3,7 @@ package it.spacecoding.javapractice;
 public class CarSimulator {
     public static void main(String[] args) {
         System.out.println("Launching car simulator ...");
+
+        Car myToyota = new Car();
     }
 }
